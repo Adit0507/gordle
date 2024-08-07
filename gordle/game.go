9d3e96f28a6e767dba340d606c8f234d8dcd3d2b
Adit0507 <adi05072002@gmail.com>
2024-08-07 14:44:49 +0530

    basic functioning

--- gordle/game.go
+++ gordle/game.go
@@ -1,15 +1,49 @@
 package gordle
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
 
-type Game struct{}
+type Game struct {
+	reader *bufio.Reader
+}
 
-func New() *Game {
-	g := &Game{}
+const solutionLength = 5
+
+func New(playerInput io.Reader) *Game {
+	g := &Game{
+		reader: bufio.NewReader(playerInput),
+	}
 
 	return g
 }
 func (g *Game) Play() {
 	fmt.Println("Welcome to gordle")
-	fmt.Printf("Enter a Guess... \n")
+	gues := g.ask()
+
+	fmt.Printf("Your guess is %s\n", string(gues))
+}
+
+func (g *Game) ask() []rune {
+	fmt.Printf("Enter a %d character guess: \n", solutionLength)
+
+	for {
+		playerInput, _, err := g.reader.ReadLine()
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "Gordle failed to read")
+			continue
+		}
+
+		guess := []rune(string(playerInput))
+
+		// verification
+		if len(guess) != solutionLength {
+			_, _ = fmt.Fprintf(os.Stderr, "Your attempt is invalid with Gordle's solution! Expected %d characters, got %d.\n", solutionLength, len(guess))
+		} else {
+			return guess
+		}
+	}
 }
