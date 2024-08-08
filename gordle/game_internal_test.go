1cb44818e5ec530df6ade4153bc6fc59e852a33c
Adit0507 <adi05072002@gmail.com>
2024-08-08 12:39:05 +0530

    test for computing feedback

--- gordle/game_internal_test.go
+++ gordle/game_internal_test.go
@@ -7,6 +7,74 @@ import (
 	"testing"
 )
 
+func TestComputeFeedback(t*testing.T){
+	tt := map[string]struct {
+		guess	string
+		solution 	string
+		expectedFeedback feedback
+	}{
+		"nominal": {
+			guess:            "HERTZ",
+			solution:         "HERTZ",
+			expectedFeedback: feedback{correctPosition, correctPosition, correctPosition, correctPosition, correctPosition},
+		},
+		"double character": {
+			guess:            "HELLO",
+			solution:         "HELLO",
+			expectedFeedback: feedback{correctPosition, correctPosition, correctPosition, correctPosition, correctPosition},
+		},
+		"double character with wrong answer": {
+			guess:            "HELLL",
+			solution:         "HELLO",
+			expectedFeedback: feedback{correctPosition, correctPosition, correctPosition, correctPosition, absentCharacter},
+		},
+		"five identical, but only two are there": {
+			guess:            "LLLLL",
+			solution:         "HELLO",
+			expectedFeedback: feedback{absentCharacter, absentCharacter, correctPosition, correctPosition, absentCharacter},
+		},
+		"two identical, but not in the right position (from left to right)": {
+			guess:            "HLLEO",
+			solution:         "HELLO",
+			expectedFeedback: feedback{correctPosition, wrongPosition, correctPosition, wrongPosition, correctPosition},
+		},
+		"three identical, but not in the right position (from left to right)": {
+			guess:            "HLLLO",
+			solution:         "HELLO",
+			expectedFeedback: feedback{correctPosition, absentCharacter, correctPosition, correctPosition, correctPosition},
+		},
+		"one correct, one incorrect, one absent (left of the correct)": {
+			guess:            "LLLWW",
+			solution:         "HELLO",
+			expectedFeedback: feedback{wrongPosition, absentCharacter, correctPosition, absentCharacter, absentCharacter},
+		},
+		"swapped characters": {
+			guess:            "HOLLE",
+			solution:         "HELLO",
+			expectedFeedback: feedback{correctPosition, wrongPosition, correctPosition, correctPosition, wrongPosition},
+		},
+		"absent character": {
+			guess:            "HULFO",
+			solution:         "HELFO",
+			expectedFeedback: feedback{correctPosition, absentCharacter, correctPosition, correctPosition, correctPosition},
+		},
+		"absent character and incorrect": {
+			guess:            "HULPP",
+			solution:         "HELPO",
+			expectedFeedback: feedback{correctPosition, absentCharacter, correctPosition, correctPosition, absentCharacter},
+		},
+	}
+
+	for name, tc := range tt {
+		t.Run(name, func(t *testing.T) {
+			fb := computeFeedback([]rune(tc.guess), []rune(tc.solution))
+			if !tc.expectedFeedback.Equal(fb) {
+			t.Errorf("guess: %q, got the wrong feedback, expected %v, got %v", tc.guess, tc.expectedFeedback, fb)
+			}
+			
+		})
+	}
+}
 func TestGameValidateAsk(t *testing.T) {
 	tt := map[string]struct {
 		word     []rune
@@ -24,7 +92,7 @@ func TestGameValidateAsk(t *testing.T) {
 
 	for name, tc := range tt {
 		t.Run(name, func(t *testing.T) {
-			g := New(nil)
+			g := New(nil, "SLICE", 0)
 
 			err := g.validateGuess(tc.word)
 			if !errors.Is(err, tc.expected) {
@@ -56,7 +124,7 @@ func TestGameAsk(t *testing.T) {
 
 	for name, tc := range tt {
 		t.Run(name, func(t *testing.T) {
-			g := New(strings.NewReader(tc.input))
+			g := New(strings.NewReader(tc.input), string(tc.want), 0)
 			got := g.ask()
 			if !slices.Equal(got, tc.want) {
 				t.Errorf("got = %v, want %v", string(got), string(tc.want))
